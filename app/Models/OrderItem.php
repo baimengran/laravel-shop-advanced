28b6204be751f8a5d740b5a42ae493df5973cada
baimengran <450175191@qq.com>
2018-09-20 10:13:33 +0000

    订单模型

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+
+
+class OrderItem extends Model
+{
+    //
+    protected $fillable = [
+        'amount',
+        'price',
+        'rating',
+        'review',
+        'reviewed_at',
+    ];
+
+
+    protected $dates = [
+        'reviewed_at'
+    ];
+
+    //不需要laravel管理时间戳字段
+    public $timestamps = false;
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+    public function productSku()
+    {
+        return $this->belongsTo(ProductSku::class);
+    }
+
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+
+}
