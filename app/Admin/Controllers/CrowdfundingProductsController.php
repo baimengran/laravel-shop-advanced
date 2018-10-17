d48fdec1b538a04c4b3b4150bb20707c5d9eb568
baimengran <450175191@qq.com>
2018-10-17 09:24:59 +0000

    管理后台 - 商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -12,9 +12,40 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class CrowdfundingProductsController extends Controller
+class CrowdfundingProductsController extends CommonProductsController
 {
-    use HasResourceActions;
+
+    public function getProductType()
+    {
+        // TODO: Implement getProductType() method.
+        return Product::TYPE_CROWDFUNDING;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        // TODO: Implement customGrid() method.
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', ' 状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
+    }
+
+    protected function customForm(Form $form)
+    {
+        // TODO: Implement customForm() method.
+        //众筹相关字段
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+    }
+
 
     /**
      * Index interface.
@@ -22,13 +53,13 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('众筹商品列表')
-            ->description('')
-            ->body($this->grid());
-    }
+//    public function index(Content $content)
+//    {
+//        return $content
+//            ->header('众筹商品列表')
+//            ->description('')
+//            ->body($this->grid());
+//    }
 
     /**
      * Show interface.
@@ -37,13 +68,13 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('description')
-            ->body($this->detail($id));
-    }
+//    public function show($id, Content $content)
+//    {
+//        return $content
+//            ->header('Detail')
+//            ->description('description')
+//            ->body($this->detail($id));
+//    }
 
     /**
      * Edit interface.
@@ -52,13 +83,13 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('编辑众筹商品')
-            ->description('')
-            ->body($this->form()->edit($id));
-    }
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('编辑众筹商品')
+//            ->description('')
+//            ->body($this->form()->edit($id));
+//    }
 
     /**
      * Create interface.
@@ -66,57 +97,57 @@ class CrowdfundingProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建众筹商品')
-            ->description('')
-            ->body($this->form());
-    }
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('创建众筹商品')
+//            ->description('')
+//            ->body($this->form());
+//    }
 
     /**
      * Make a grid builder.
      *
      * @return Grid
      */
-    protected function grid()
-    {
-        $grid = new Grid(new Product);
-
-        //只展示type为众筹类型的商品
-        $grid->model()->where('type', Product::TYPE_CROWDFUNDING);
-        $grid->id('ID')->sortable();
-        //$grid->type('Type');
-        //$grid->category_id('Category id');
-        $grid->title('商品名称');
-        //$grid->description('Description');
-        //$grid->image('Image');
-        $grid->on_sale('已上架')->display(function ($value) {
-            return $value ? '是' : '否';
-        });
-        //$grid->rating('Rating');
-        //$grid->sold_count('Sold count');
-        //$grid->review_count('Review count');
-        $grid->price('价格');
-        //$grid->created_at('Created at');
-        //$grid->updated_at('Updated at');
-        $grid->column('crowdfunding.target_amount', '目标金额');
-        $grid->column('crowdfunding.end_at', '结束时间');
-        $grid->column('crowdfunding.total_amount', '当前金额');
-        $grid->column('crowdfunding.status', '状态')->display(function ($value) {
-            return CrowdfundingProduct::$statusMap[$value];
-        });
-        $grid->actions(function ($actions) {
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-        $grid->tools(function ($tools) {
-            $tools->batch(function ($batch) {
-                $batch->disableDelete();
-            });
-        });
-        return $grid;
-    }
+//    protected function grid()
+//    {
+//        $grid = new Grid(new Product);
+//
+//        //只展示type为众筹类型的商品
+//        $grid->model()->where('type', Product::TYPE_CROWDFUNDING);
+//        $grid->id('ID')->sortable();
+//        //$grid->type('Type');
+//        //$grid->category_id('Category id');
+//        $grid->title('商品名称');
+//        //$grid->description('Description');
+//        //$grid->image('Image');
+//        $grid->on_sale('已上架')->display(function ($value) {
+//            return $value ? '是' : '否';
+//        });
+//        //$grid->rating('Rating');
+//        //$grid->sold_count('Sold count');
+//        //$grid->review_count('Review count');
+//        $grid->price('价格');
+//        //$grid->created_at('Created at');
+//        //$grid->updated_at('Updated at');
+//        $grid->column('crowdfunding.target_amount', '目标金额');
+//        $grid->column('crowdfunding.end_at', '结束时间');
+//        $grid->column('crowdfunding.total_amount', '当前金额');
+//        $grid->column('crowdfunding.status', '状态')->display(function ($value) {
+//            return CrowdfundingProduct::$statusMap[$value];
+//        });
+//        $grid->actions(function ($actions) {
+//            $actions->disableView();
+//            $actions->disableDelete();
+//        });
+//        $grid->tools(function ($tools) {
+//            $tools->batch(function ($batch) {
+//                $batch->disableDelete();
+//            });
+//        });
+//        return $grid;
+//    }
 
     /**
      * Make a show builder.
@@ -124,64 +155,64 @@ class CrowdfundingProductsController extends Controller
      * @param mixed $id
      * @return Show
      */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->type('Type');
-        $show->category_id('Category id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
+//    protected function detail($id)
+//    {
+//        $show = new Show(Product::findOrFail($id));
+//
+//        $show->id('Id');
+//        $show->type('Type');
+//        $show->category_id('Category id');
+//        $show->title('Title');
+//        $show->description('Description');
+//        $show->image('Image');
+//        $show->on_sale('On sale');
+//        $show->rating('Rating');
+//        $show->sold_count('Sold count');
+//        $show->review_count('Review count');
+//        $show->price('Price');
+//        $show->created_at('Created at');
+//        $show->updated_at('Updated at');
+//
+//        return $show;
+//    }
 
     /**
      * Make a form builder.
      *
      * @return Form
      */
-    protected function form()
-    {
-        $form = new Form(new Product);
-
-        //在表单中添加一个名为type，值为Product::TYPE_CROWDFUNDING的隐藏字段
-        $form->hidden('type')->value(Product::TYPE_CROWDFUNDING);
-        $form->select('category_id', '类目')->options(function ($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-        $form->text('title', '商品名称')->rules('required');
-        $form->editor('description', '商品描述')->rules('required');
-        $form->image('image', '封面图片')->rules('required|image');
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
-        //添加众筹相关字段
-        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
-        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
-        $form->hasMany('skus', '商品 SKU', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-        });
-
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))
-                ->where(Form::REMOVE_FLAG_NAME, 0)
-                ->min('price');
-        });
-
-        return $form;
-    }
+//    protected function form()
+//    {
+//        $form = new Form(new Product);
+//
+//        //在表单中添加一个名为type，值为Product::TYPE_CROWDFUNDING的隐藏字段
+//        $form->hidden('type')->value(Product::TYPE_CROWDFUNDING);
+//        $form->select('category_id', '类目')->options(function ($id) {
+//            $category = Category::find($id);
+//            if ($category) {
+//                return [$category->id => $category->full_name];
+//            }
+//        })->ajax('/admin/api/categories?is_directory=0');
+//        $form->text('title', '商品名称')->rules('required');
+//        $form->editor('description', '商品描述')->rules('required');
+//        $form->image('image', '封面图片')->rules('required|image');
+//        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default(0);
+//        //添加众筹相关字段
+//        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+//        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+//        $form->hasMany('skus', '商品 SKU', function (Form\NestedForm $form) {
+//            $form->text('title', 'SKU 名称')->rules('required');
+//            $form->text('description', 'SKU 描述')->rules('required');
+//            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+//            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+//        });
+//
+//        $form->saving(function (Form $form) {
+//            $form->model()->price = collect($form->input('skus'))
+//                ->where(Form::REMOVE_FLAG_NAME, 0)
+//                ->min('price');
+//        });
+//
+//        return $form;
+//    }
 }
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -11,9 +11,36 @@ use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Show;
 
-class ProductsController extends Controller
+class ProductsController extends CommonProductsController
 {
-    use HasResourceActions;
+    public function getProductType()
+    {
+        // TODO: Implement getProductType() method.
+        return Product::TYPE_NORMAL;
+    }
+
+
+    protected function customGrid(Grid $grid)
+    {
+        // TODO: Implement customGrid() method.
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // TODO: Implement customForm() method.
+        //普通商品没有额外字段，实现方法无需写代码
+    }
+
 
     /**
      * Index interface.
@@ -21,13 +48,13 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function index(Content $content)
-    {
-        return $content
-            ->header('商品列表')
-            ->description('')
-            ->body($this->grid());
-    }
+//    public function index(Content $content)
+//    {
+//        return $content
+//            ->header('商品列表')
+//            ->description('')
+//            ->body($this->grid());
+//    }
 
     /**
      * Show interface.
@@ -36,13 +63,13 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function show($id, Content $content)
-    {
-        return $content
-            ->header('Detail')
-            ->description('')
-            ->body($this->detail($id));
-    }
+//    public function show($id, Content $content)
+//    {
+//        return $content
+//            ->header('Detail')
+//            ->description('')
+//            ->body($this->detail($id));
+//    }
 
     /**
      * Edit interface.
@@ -51,13 +78,13 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function edit($id, Content $content)
-    {
-        return $content
-            ->header('商品编辑')
-            ->description('')
-            ->body($this->form()->edit($id));
-    }
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('商品编辑')
+//            ->description('')
+//            ->body($this->form()->edit($id));
+//    }
 
     /**
      * Create interface.
@@ -65,57 +92,57 @@ class ProductsController extends Controller
      * @param Content $content
      * @return Content
      */
-    public function create(Content $content)
-    {
-        return $content
-            ->header('创建商品')
-            ->description('')
-            ->body($this->form());
-    }
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('创建商品')
+//            ->description('')
+//            ->body($this->form());
+//    }
 
     /**
      * Make a grid builder.
      *
      * @return Grid
      */
-    protected function grid()
-    {
-        $grid = new Grid(new Product);
-
-        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
-        $grid->model()->with(['category']);
-        $grid->id('Id')->sortable();
-        $grid->title('商品名称');
-        $grid->column('category.name', '类目');
-        //$grid->description('Description');
-        //$grid->image('Image');
-        $grid->on_sale('已上架')->display(function ($value) {
-            return $value ? '是' : '否';
-        });
-        $grid->price('价格');
-        $grid->rating('评分');
-        $grid->sold_count('销量');
-        $grid->review_count('评论数');
-
-//        $grid->created_at('Created at');
-////        $grid->updated_at('Updated at');
-        //修改动作
-        $grid->actions(function ($actions) {
-            //不在每一行后展示查看
-            $actions->disableView();
-            //不在每一行后展示删除
-            $actions->disableDelete();
-        });
-        //批量工具
-        $grid->tools(function ($tools) {
-            //禁用批量删除按钮
-            $tools->batch(function ($batch) {
-                $batch->disableDelete();
-            });
-        });
-
-        return $grid;
-    }
+//    protected function grid()
+//    {
+//        $grid = new Grid(new Product);
+//
+//        $grid->model()->where('type', Product::TYPE_NORMAL)->with(['category']);
+//        $grid->model()->with(['category']);
+//        $grid->id('Id')->sortable();
+//        $grid->title('商品名称');
+//        $grid->column('category.name', '类目');
+//        //$grid->description('Description');
+//        //$grid->image('Image');
+//        $grid->on_sale('已上架')->display(function ($value) {
+//            return $value ? '是' : '否';
+//        });
+//        $grid->price('价格');
+//        $grid->rating('评分');
+//        $grid->sold_count('销量');
+//        $grid->review_count('评论数');
+//
+////        $grid->created_at('Created at');
+//////        $grid->updated_at('Updated at');
+//        //修改动作
+//        $grid->actions(function ($actions) {
+//            //不在每一行后展示查看
+//            $actions->disableView();
+//            //不在每一行后展示删除
+//            $actions->disableDelete();
+//        });
+//        //批量工具
+//        $grid->tools(function ($tools) {
+//            //禁用批量删除按钮
+//            $tools->batch(function ($batch) {
+//                $batch->disableDelete();
+//            });
+//        });
+//
+//        return $grid;
+//    }
 
     /**
      * Make a show builder.
@@ -123,75 +150,75 @@ class ProductsController extends Controller
      * @param mixed $id
      * @return Show
      */
-    protected function detail($id)
-    {
-        $show = new Show(Product::findOrFail($id));
-
-        $show->id('Id');
-        $show->title('Title');
-        $show->description('Description');
-        $show->image('Image');
-        $show->on_sale('On sale');
-        $show->rating('Rating');
-        $show->sold_count('Sold count');
-        $show->review_count('Review count');
-        $show->price('Price');
-        $show->created_at('Created at');
-        $show->updated_at('Updated at');
-
-        return $show;
-    }
+//    protected function detail($id)
+//    {
+//        $show = new Show(Product::findOrFail($id));
+//
+//        $show->id('Id');
+//        $show->title('Title');
+//        $show->description('Description');
+//        $show->image('Image');
+//        $show->on_sale('On sale');
+//        $show->rating('Rating');
+//        $show->sold_count('Sold count');
+//        $show->review_count('Review count');
+//        $show->price('Price');
+//        $show->created_at('Created at');
+//        $show->updated_at('Updated at');
+//
+//        return $show;
+//    }
 
     /**
      * Make a form builder.
      *
      * @return Form
      */
-    protected function form()
-    {
-        //创建一个表单
-        $form = new Form(new Product);
-
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-        //创建一个输入框，第一个参数title是模型的字段名，第二个参数是该字段描述
-        $form->text('title', '商品名称')->rules('required');
-        //$form->textarea('description', 'Description');
-        //创建选择图片框,生成MD5随机文件名，并修改上传目录
-
-        //创建类目字段，使用ajax方式搜索添加
-        $form->select('category_id', '类目')->options(function ($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->full_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-        $form->image('image', '封面图片')->uniqueName()->move('products');
-        //创建选择图片框
-        // $form->image('image', '封面图片')->rules('required|image');
-        //创建一个富文本编辑器,默认laravel->admin禁用了editor表单组件，可在app/Admin/bootstrap。php内将‘editor’删除
-        $form->editor('description', '商品描述')->rules('required');
-        //创建一个单选框
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-        //$form->switch('on_sale', 'On sale')->default(1);
-//        $form->decimal('rating', 'Rating')->default(5.00);
-//        $form->number('sold_count', 'Sold count');
-//        $form->number('review_count', 'Review count');
-//        $form->decimal('price', 'Price');
-        //添加一对多关联模型，关联SKU信息
-        $form->hasMany('skus', 'SKU列表', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-        });
-
-        //定义事件回调，当模型即将保存时会触发这个回调我们需要在保存商品之前拿到所有 SKU 中最低的价格作为商品的价格，然后通过 $form->model()->price 存入到商品模型中。
-        $form->saving(function (Form $form) {
-            //collect() 函数是 Laravel 提供的一个辅助函数，可以快速创建一个 Collection 对象。把用户提交上来的 SKU 数据放到 Collection 中，
-            //利用 Collection 提供的 min() 方法求出所有 SKU 中最小的 price，后面的 ?: 0 则是保证当 SKU 数据为空时 price 字段被赋值 0。
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-        });
-
-        return $form;
-    }
+//    protected function form()
+//    {
+//        //创建一个表单
+//        $form = new Form(new Product);
+//
+//        $form->hidden('type')->value(Product::TYPE_NORMAL);
+//        //创建一个输入框，第一个参数title是模型的字段名，第二个参数是该字段描述
+//        $form->text('title', '商品名称')->rules('required');
+//        //$form->textarea('description', 'Description');
+//        //创建选择图片框,生成MD5随机文件名，并修改上传目录
+//
+//        //创建类目字段，使用ajax方式搜索添加
+//        $form->select('category_id', '类目')->options(function ($id) {
+//            $category = Category::find($id);
+//            if ($category) {
+//                return [$category->id => $category->full_name];
+//            }
+//        })->ajax('/admin/api/categories?is_directory=0');
+//        $form->image('image', '封面图片')->uniqueName()->move('products');
+//        //创建选择图片框
+//        // $form->image('image', '封面图片')->rules('required|image');
+//        //创建一个富文本编辑器,默认laravel->admin禁用了editor表单组件，可在app/Admin/bootstrap。php内将‘editor’删除
+//        $form->editor('description', '商品描述')->rules('required');
+//        //创建一个单选框
+//        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+//        //$form->switch('on_sale', 'On sale')->default(1);
+////        $form->decimal('rating', 'Rating')->default(5.00);
+////        $form->number('sold_count', 'Sold count');
+////        $form->number('review_count', 'Review count');
+////        $form->decimal('price', 'Price');
+//        //添加一对多关联模型，关联SKU信息
+//        $form->hasMany('skus', 'SKU列表', function (Form\NestedForm $form) {
+//            $form->text('title', 'SKU 名称')->rules('required');
+//            $form->text('description', 'SKU 描述')->rules('required');
+//            $form->text('price', '单价')->rules('required|numeric|min:0.01');
+//            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+//        });
+//
+//        //定义事件回调，当模型即将保存时会触发这个回调我们需要在保存商品之前拿到所有 SKU 中最低的价格作为商品的价格，然后通过 $form->model()->price 存入到商品模型中。
+//        $form->saving(function (Form $form) {
+//            //collect() 函数是 Laravel 提供的一个辅助函数，可以快速创建一个 Collection 对象。把用户提交上来的 SKU 数据放到 Collection 中，
+//            //利用 Collection 提供的 min() 方法求出所有 SKU 中最小的 price，后面的 ?: 0 则是保证当 SKU 数据为空时 price 字段被赋值 0。
+//            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+//        });
+//
+//        return $form;
+//    }
 }
