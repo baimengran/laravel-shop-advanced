5c8bf5d726cc8c1b40cbdf2d058a744d147162ce
baimengran <450175191@qq.com>
2018-10-26 09:12:17 +0000

    同步商品数据至 Elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -10,6 +10,7 @@ namespace App\Admin\Controllers;
 
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SyncOneProductToES;
 use App\Models\Category;
 use App\Models\Product;
 use Encore\Admin\Controllers\HasResourceActions;
@@ -112,6 +113,11 @@ abstract class CommonProductsController extends Controller
         });
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+        });
+        //触发更新elasticsearch队列
+        $form->saved(function (Form $form) {
+            $product = $form->model();
+            $this->dispatch(new SyncOneProductToES($product));
         });
 
         return $form;
